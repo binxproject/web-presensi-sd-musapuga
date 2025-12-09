1d175f5f9fde1e2bcf0e09d9cd535f5c2021b220
binxproject <iconiclampard99@gmail.com>
2025-12-09 17:57:32 +0700

    setup css

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+header('Location: karyawan/presensi.php');
+
+
+?>
---
+++ karyawan/presensi.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presensi</title>
+    <link rel="stylesheet" href="../src/output.css">
+
+
+</head>
+<body>
+    
+
+
+
+
+
+</body>
+</html>
